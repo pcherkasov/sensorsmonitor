709578dc7328fe5b6c8352bcd6501f23c119664d
Pavel Charkasau <p.charkasau@godeltech.com>
2020-05-07 16:52:35 +0300

    add npm add angular.json change database

--- src/main/java/com/labinvent/cherkasov/config/security/SecurityConfig.java
+++ src/main/java/com/labinvent/cherkasov/config/security/SecurityConfig.java
@@ -19,22 +19,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/index", "/login").permitAll()
-                .antMatchers("/api/sensor/**").authenticated();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/api/sensor/**").authenticated();
+//    }
 
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+//        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
         auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}1111").roles("VIEWER")
+                .withUser("user").password("$2y$10$v2m2sGcDECgUq9FjlO622.gzPx1RlJPN1kFnzKEu7N5XNHWTTXzY6").roles("VIEWER")
                 .and()
                 .withUser("admin").password("{noop}1111").roles("ADMINISTRATOR");
     }
